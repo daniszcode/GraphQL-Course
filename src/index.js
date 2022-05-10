7a66cf52c81f9f56106e1899d41d1dd60102565c
daniszcode <daniele.marques@creditas.com>
2022-05-10 16:31:50 -0300

    subindo servidor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('ola');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+    }
+  `,
+});
+
+server.listen(4000).then(({ url }) => {
+  console.log(`server listening to ${url}`);
+});
