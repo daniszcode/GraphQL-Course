5a81860491379c538ec1a8be4132cba8c2ee5714
daniszcode <daniele.marques@creditas.com>
2022-05-13 13:11:35 -0300

    refactor: Addition resolvers at type query

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       hello: String
+      hi: String
     }
   `,
+
+  resolvers: {
+    Query: {
+      hello: () => {
+        return 'did i right?';
+      },
+      hi: () => {
+        return 'other query for here';
+      },
+    },
+  },
 });
 
 server.listen(4000).then(({ url }) => {
